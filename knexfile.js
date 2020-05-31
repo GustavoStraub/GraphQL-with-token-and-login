7c4c00b90f4de1a28311f1cd58de1a4ae4a8e1f5
Gustavo Straub <gustavostraubcovers@gmail.com>
2020-05-30 21:41:19 -0300

    .env created

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,13 @@
-// Update with your config settings.
+require('dotenv').config()
 
 module.exports = {
   client: 'mysql',
   connection: {
-    database: 'projeto-final',
-    user:     'root',
-    password: '12345678'
+    host: process.env.APP_DB_HOST,
+    port: process.env.APP_DB_PORT,
+    database: process.env.APP_DB_NAME,
+    user:  process.env.APP_DB_USER,
+    password: process.env.APP_DB_PASSWORD
   },
   pool: {
     min: 2,
